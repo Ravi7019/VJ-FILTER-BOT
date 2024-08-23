415c5a0d8999ca0ee768f32c10ef603866a49b64
Ravi7019 <156110174+Ravi7019@users.noreply.github.com>
2024-08-23 12:09:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-100
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://KannadaMagaa:KannadaMagaa@kannadamagaaclone.kweqbmy.mongodb.net/?retryWrites=true&w=majority&appName=KannadaMagaaClone")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://KCS:KCS@cluster0.dccan.mongodb.net/")
 DATABASE_NAME = environ.get('DATABASE_NAME', "ravikumar1cn")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
